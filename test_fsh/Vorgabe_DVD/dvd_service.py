39947722aaf71f28d4c3b35117bc3cf24bef0a85
Frank Schmitfranz <masterx29@gmx.de>
2024-09-23 13:29:40 +0200

    mal sehen

--- test_fsh/Vorgabe_DVD/dvd_service.py
+++ test_fsh/Vorgabe_DVD/dvd_service.py
@@ -1,2 +1,16 @@
-class DVDsService:
-    pass
+from dvdp import DVD
+
+class DVDService:
+    def __init__(self) -> None:
+        self.dvd = dict()
+    def create(self, id, name, description):
+        if id == None or name == None or description == None:
+            raise Exception(f'alle paramter muessen gestzt sein')
+        if id in self.dvds.keys():
+            raise Exception(f'{id} is nicht da' )
+        new_dvd = DVD(id, name, description)
+        self.dvd[id] = new_dvd
+    def find_by(self, id):
+        if id == None:
+            raise Exception(f'id muss schon sein!')
+        return self.dvds.get(id)
