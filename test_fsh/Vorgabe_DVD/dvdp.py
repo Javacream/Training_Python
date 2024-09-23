9238f8f2e7012642e9dfb6751a87d61fabfb11f6
Frank Schmitfranz <masterx29@gmx.de>
2024-09-23 13:19:53 +0200

    ispire from sawatzki ;-)

--- test_fsh/Vorgabe_DVD/dvdp.py
+++ test_fsh/Vorgabe_DVD/dvdp.py
@@ -1,2 +1,11 @@
 class DVD:
-    pass
+    def __init__(self, id, name, description) -> None:
+        self.id = id
+        self.name = name
+        self.description = description
+    def __repr__(self) -> str:
+        return f'DVD: id={self.id}, name={self.name}, description={self.description}'
+    def __eq__(self, other) -> bool:
+        return self.id == other.id and self.name == other.name and self.description == other.description    
+    def __eq__(self, other) -> bool:
+        return hash(self.id)
